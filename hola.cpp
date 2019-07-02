128f7ca742dab405286da7aaf1404057ef698dc3
Gerardo Medrano <gmedrano@auronix.com>
2019-07-02 18:44:50 -0500

    se agrego el archivo hola.cpp

---
+++ hola.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+void reverse(char text[]){
+	for(int i = 0; i<=strlen(text); i++){
+        printf("%c",text[strlen(text)-i]);   
+    }
+}
+void Numeros(int final){
+	for(int i = 1; i<=final;i++){
+		if(i%3 ==0 )
+			printf("Tres");
+
+		if(i%5==0)
+			printf("Cinco");
+		if(i%3 != 0 && i%5 != 0){
+			for(int j = 0; j < i ; j++)
+				printf("%d ",i);
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	//hola
+    char text[] = "Hello world!";
+    printf("%s\n",text);
+	reverse(text);
+    return 0;
+}
